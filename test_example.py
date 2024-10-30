9d91fa7fe17fcbb180c3cfe8f431de312fd9808b
shirinova-ra <120368784+shirinova-ra@users.noreply.github.com>
2024-10-30 23:37:50 +0300

    Update test_example.py

--- test_example.py
+++ test_example.py
@@ -4,22 +4,13 @@ import pandas as pd
 from file import generate_data
 
 def test_generate_data():
-    """Тестирует функцию генерации данных."""
     df = generate_data(num_points=100)
-
-    # Проверяем, что возвращается DataFrame
+    
     assert isinstance(df, pd.DataFrame), "Должен вернуть DataFrame"
-    
-    # Проверяем, что у DataFrame 100 строк
     assert len(df) == 100, "Количество строк должно быть 100"
-    
-    # Проверяем, что в DataFrame есть нужные колонки
     assert 'x' in df.columns, "Колонка 'x' должна быть в DataFrame"
     assert 'y' in df.columns, "Колонка 'y' должна быть в DataFrame"
-    
-    # Проверяем, что значения в колонке 'x' в правильном диапазоне
     assert (df['x'].min() >= 0) and (df['x'].max() <= 10), "Значения 'x' должны быть в диапазоне от 0 до 10"
 
-# Запуск тестов
 if __name__ == "__main__":
     pytest.main()
